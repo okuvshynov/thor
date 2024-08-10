00d545defb5e713e7d5e3e994fccb67bc61fda1c
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2024-08-10 12:47:18 -0400

    thor: 1. hello world

---
+++ thor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Define colors
+HELLO_FG="#[fg=colour231]" # White
+HELLO_BG="#[bg=colour16]"  # Black
+WORLD_FG="#[fg=colour40]"  # Green
+WORLD_BG="#[bg=colour232]" # Dark Gray
+
+# Output the formatted string
+echo "${HELLO_FG}${HELLO_BG}hello${WORLD_FG}${WORLD_BG} world"
