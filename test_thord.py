a3dfd5e21bf5dfdbe1ceeb7edea54bbfc7c71822
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2024-08-18 18:48:36 -0400

    thord: unitest

---
+++ test_thord.py
@@ -0,0 +1,20 @@
+import unittest
+
+from thord import generate_random_id
+
+
+class TestGenerateRandomID(unittest.TestCase):
+    def test_length(self):
+        length = 10
+        generated_id = generate_random_id(length)
+        self.assertEqual(len(generated_id), length)
+
+    def test_uniqueness(self):
+        length = 8
+        id1 = generate_random_id(length)
+        id2 = generate_random_id(length)
+        self.assertNotEqual(id1, id2)
+
+
+if __name__ == '__main__':
+    unittest.main()
